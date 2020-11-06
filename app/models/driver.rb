3d0ba308b141a8730fab57dbc7e9884c1c2816a5
Pauline Chane <pauline.chane@gmail.com>
2020-11-05 19:43:04 -0800

    changed mind about smart dispatching. maybe try after the project is finished? (would be nice though)

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -44,35 +44,4 @@ class Driver < ApplicationRecord
   def clear_trips
     Trip.destroy_all(driver_id: self.id)
   end
-
-  def self.most_available
-    # find all available
-    available_drivers = Driver.find_all(available: true)
-    return -1 if available_drivers.empty?
-    # find drivers with no in-progress trips
-    no_ip_drivers = []
-    available_drivers.each do |driver|
-      nil_trip = false
-      # check for trips with nil
-      driver.trips.each do |trip|
-        nil_trip = true if trip.rating.nil?
-      end
-      # only shovel is !nil_trip
-      no_ip_drivers << driver unless nil_trip
-    end
-    return -1 if available_drivers.empty?
-
-    # find drivers with no trips
-    idle_driver = no_ip_drivers.find { |driver| driver.trips.empty? }
-    # if result nil, all drivers have at least one trip, must compare for most "stale"
-    if idle_driver.nil?
-      idle_driver = no_ip_drivers[0]
-      driver_last_trip = idle_driver.trips.max_by(&:date)
-      no_ip_drivers.each do |driver|
-        last_trip = driver.trips.max_by{|trip| Date.parse(trip.date)}
-        idle_driver = driver if last_trip.end_time < driver_last_trip.end_time
-      end
-    end
-    return idle_driver
-  end
 end
